2822bda9832af5bc365a3c67d5fd7df5e21b77f3
eclipseop <eclipseop@420blaze.it>
2020-02-26 13:12:20 -0500

    weather

--- weather.js
+++ weather.js
@@ -3,12 +3,20 @@ var weather = require('weather-js')
 module.exports = {
     getTemp: function(callback) {
         weather.find({ search: 'Grand Blanc, MI', degreeType: 'F' }, function(err, res) {
-            callback(res[0].current.temperature)
+            if (res == undefined) {
+                callback('0');
+            } else {
+                callback(res[0].current.temperature)
+            }
         });
     },
     getSkytext: function(callback) {
         weather.find({ search: 'Grand Blanc, MI', degreeType: 'F' }, function(err, res) {
-            callback(res[0].current.skytext)
+            if (res == undefined) {
+                callback('0');
+            } else {
+                callback(res[0].current.skytext)
+            }
         });
     }
 }
